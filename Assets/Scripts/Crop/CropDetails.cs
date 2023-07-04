7ef0b0934130496859e373222ffff2aa4cffeb30
Enlighten <1196301544@qq.com>
2023-07-04 16:51:59 +0800

    V13.1

---
+++ Assets/Scripts/Crop/CropDetails.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CropDetails
+{
+    [ItemCodeDescription] public int seedItemCode; //种子物体的ID
+    
+    public int[] growthDays; //每个阶段的生长天数
+    public int totalGrowthDays; // 总生长天数
+    public GameObject[] growthPrefab;// 每个生长阶段的预制体
+    public Sprite[] growthSprite; // 每个生长阶段的精灵
+    public Season[] seasons; // 指明可以生长于哪些季节，用不到
+    
+    public Sprite harvestedSprite; // 可以收获时的精灵
+    
+    [ItemCodeDescription] public int harvestedTransformItemCode; //收获后转化为的物体ID，不一定会有。如树被砍伐了转化为树桩
+    
+    //在收获动画以前，指明作物是否被禁用，因为作物其实被启用了会被直接拾取
+    public bool hideCropBeforeHarvestedAnimation;  
+    //在收获动画以前，指明作物碰撞体是否被禁用
+    public bool disableCropCollidersBeforeHarvestedAnimation; 
+    //是否在最后阶段存在被收获动画，如树被砍伐的时候会摇晃
+    public bool isHarvestedAnimation; 
+    //是否在玩家的位置生成作物
+    public bool spawnCropProducedAtPlayerPosition;
+   
+    public bool isHarvestActionEffect = false; // // 是否在收割动作时具有粒子效果
+    public HarvestActionEffect harvestActionEffect; // 指明哪种粒子效果
+    
+    [ItemCodeDescription] public int[] harvestToolItemCode; //指明用来收获的工具的类型，如果是0，则表明不用工具
+    public int[] requiredHarvestActions; ////根据harvestToolItemCode，相关的收获动作number
+    
+    [ItemCodeDescription] public int[] cropProducedItemCode; //收获的物体的ID
+
+    //// 收获的最小和最大作物数量，如果不一致，返回其间的随机数
+    public int[] cropProducedMinQuantity; 
+    public int[] cropProducedMaxQuantity; 
+    
+    //收获以后下次收获还需几天，-1则表明是一次性作物
+    public int daysToRegrow;
+
+
+    /// <summary>
+    /// 若工具ID可以用来收获这个作物则返回true
+    /// </summary>
+    public bool CanUseToolToHarvestCrop(int toolItemCode)
+    {
+        if (RequiredHarvestActionsForTool(toolItemCode) == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+
+
+    /// <summary>
+    /// returns -1 if the tool can't be used to harvest this crop, else returns the number of harvest actions required by this tool
+    /// </summary>
+    public int RequiredHarvestActionsForTool(int toolItemCode)
+    {
+        for (int i = 0; i < harvestToolItemCode.Length; i++)
+        {
+            if (harvestToolItemCode[i] == toolItemCode)
+            {
+                return requiredHarvestActions[i];
+            }
+        }
+        return -1;
+    }
+}
+
